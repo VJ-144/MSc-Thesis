6c9f1ab83f5808c5f4e496f479f7d2222819134a
Vijay Chand <VijayChand144@Gmail.com>
2023-02-06 23:23:52 +0000

    Fix event loop parameter

--- MainCode/Data_Analysis/Analyze_data/run_genie_analysis.C
+++ MainCode/Data_Analysis/Analyze_data/run_genie_analysis.C
@@ -184,7 +184,7 @@ int main(int argc, char **argv)
   }
 
   genie_analysis  t( target, beam_en, rotations, choice, elSectors_flag, deltaPhiEl, thetaEl_lb, thetaEl_ub, elMom_lb, protSectors_flag, deltaPhiProt, thetaProt_lb, thetaProt_ub, protMom_lb, protMom_ub, NumOfProton, detector_acceptance);
-  t.Loop(tuple <bool, bool, bool, bool> InputParam);
+  t.Loop(InputParam);
 
   return 0;
 }
