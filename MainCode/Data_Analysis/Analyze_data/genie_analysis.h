305ef85ca2caa16887deaa9ed778f67c71b3d214
Vijay Chand <VijayChand144@Gmail.com>
2023-02-06 23:35:41 +0000

    Revert Bool Arguments

--- MainCode/Data_Analysis/Analyze_data/genie_analysis.h
+++ MainCode/Data_Analysis/Analyze_data/genie_analysis.h
@@ -273,7 +273,7 @@ public :
    virtual Int_t    GetEntry(Long64_t entry);
    virtual Long64_t LoadTree(Long64_t entry);
    virtual void     Init(TTree *tree);
-   virtual void     Loop(tuple <bool, bool, bool, bool> InputParam);
+   virtual void     Loop();
    virtual Bool_t   Notify();
    virtual void     Show(Long64_t entry = -1);
 
