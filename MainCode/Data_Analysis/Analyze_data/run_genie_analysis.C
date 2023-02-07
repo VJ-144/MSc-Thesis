b0602e2e14366a08fe122c45430a55d12e3a1d73
Vijay Chand <VijayChand144@Gmail.com>
2023-02-07 00:20:53 +0000

    testing InputParameters

--- MainCode/Data_Analysis/Analyze_data/run_genie_analysis.C
+++ MainCode/Data_Analysis/Analyze_data/run_genie_analysis.C
@@ -26,8 +26,8 @@ int main(int argc, char **argv)
   std::cout << std::endl; // separate this output from the rest of the output
 
   // vchand: editing if(argc<15) as the parameters are input in file now
-  bool discription = discription_in;
-  if( discription == true ){
+  int discription = discription_in;
+  if( discription == 1){
     std::cout << "/-------------------------------------------------------\\" << std::endl;
     std::cout << "| Please specify the target (3He, 56Fe, C12, 4He), the  |" << std::endl;
     std::cout << "| beam energy (1161, 2261 or 4461), the data type       |" << std::endl;
